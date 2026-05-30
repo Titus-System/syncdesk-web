8002c7565f792d0208dbc759d87c6b210fbf99e7
Maria Fernanda <fernandinha.mf30@gmail.com>
2026-05-30 11:01:58 -0300

    chore: resolve conflitos de merge com a main

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@titus-system/syncdesk': path.resolve(__dirname, '../syncdesk-library/src')
     },
-    dedupe: ['react', 'react-dom']
+    dedupe: ['react', 'react-dom'],
   },
   optimizeDeps: {
-    include: ['react-use-websocket']
-  }
-})
+    include: ['react-use-websocket'],
+  },
+});
